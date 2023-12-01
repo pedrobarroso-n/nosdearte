f4202075f1c26ea5a42d38d46fbd17ebabe9a856
Pedro Barroso <pbn62413@gmail.com>
2023-12-01 15:36:23 -0300

    finalizacoes no footer

---
+++ js/script.js
@@ -0,0 +1,5 @@
+addEventListener('click', ()=> { 
+    document.getElementsByTagName('a')[0].setAttribute('href', './index.html')  //link para page inicio
+    document.getElementsByTagName('a')[1].setAttribute('href', './produtos.html') //link para a page produtos
+    document.getElementsByTagName('a')[2].setAttribute('href', './contato.html')  //link para a page contato
+})
