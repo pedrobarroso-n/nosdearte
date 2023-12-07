5a21950d7668d3a9d33465bacadfc72f2c87e143
Pedro Barroso <pbn62413@gmail.com>
2023-12-07 14:54:52 -0300

    finalizai a parte dos slides

--- js/script.js
+++ js/script.js
@@ -3,18 +3,3 @@ addEventListener('click', ()=> {
     document.getElementsByTagName('a')[1].setAttribute('href', './produtos.html') //link para a page produtos
     document.getElementsByTagName('a')[2].setAttribute('href', './contato.html')  //link para a page contato
 })
-
-const banner = document.getElementById('banner') /*tag dos banner*/
-function slide_1(){
-    banner.setAttribute('src', '../image/slide_1.gif')
-    setTimeout(slide_2, 10000)
-}
-function slide_2(){
-    banner.setAttribute('src', '../image/slide_2.gif')
-    setTimeout(slide_3, 10000)
-}
-function slide_3(){
-    banner.setAttribute('src', '../image/slide_3.gif')
-    setTimeout(slide_1, 10000)
-}
-    
