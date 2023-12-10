41f8549d4e4c8e6b2c290a9f9d965994d9f04947
Pedro Barroso <pbn62413@gmail.com>
2023-12-10 13:53:29 -0300

    update 10.12.23

--- js/script.js
+++ js/script.js
@@ -3,17 +3,7 @@ if (document.body.clientWidth < 768){ Menu() }
 function Menu(){
     const nav = document.querySelector('nav')
     const ul = document.querySelector('ul')
-
     const menu = document.createElement('menu')
-    menu.style.width = '36px'
-    menu.style.height = '32px'
-    menu.style.position = 'relative'
-    menu.style.top = '-55%'
-    menu.style.backgroundImage = 'url(../image/menu-icon.png)'
-    menu.style.backgroundRepeat = 'no-repeat'
-    menu.style.backgroundPosition = 'center'
-    menu.style.backgroundSize = 'cover'
-    menu.style.transition = '.3s ease'
     nav.appendChild(menu)
     
     menu.addEventListener('click', ()=> {
