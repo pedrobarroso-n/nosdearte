c14c48b9649014e8b3febaa8a1d76c11df94fd22
Pedro Barroso <pbn62413@gmail.com>
2023-12-16 12:44:48 -0300

    update day 16.12.23

--- js/script.js
+++ js/script.js
@@ -16,3 +16,98 @@ function Menu(){
         }
     })
 }
+
+const products = [
+    {
+        name: 'Bolinhas decorativas de biscuit',
+        price: '0, 00',
+        image: '../image/bolinhas.jpeg'
+    },
+    {
+        name: 'Bolsa de crochê decorada',
+        price: '0, 00',
+        image: '../image/bolsa.jpeg'
+    },
+    {
+        name: 'Chaveiro personalizado de biscuit',
+        price: '0, 00',
+        image: '../image/chaveiro.jpeg'
+    },
+    {
+        name: 'Top de crochê',
+        price: '0, 00',
+        image: '../image/top.jpeg'
+    },
+    {
+        name: 'Bonequinhas de biscuit',
+        price: '0, 00',
+        image: '../image/bonequinhas.jpeg'
+    },
+    {
+        name: 'Capa para almofada (Branca)',
+        price: '0, 00',
+        image: '../image/almofada-1.jpeg'
+    },
+    {
+        name: 'Capa para almofada (Vermelha)',
+        price: '0, 00',
+        image: '../image/almofada-2.jpeg'
+    },
+    {
+        name: 'Conjunto de tapetes',
+        price: '0, 00',
+        image: '../image/conjunto_tapetes.jpeg'
+    },
+    {
+        name: 'Boneca de pano',
+        price: '0, 00',
+        image: '../image/boneca_pano.jpeg'
+    },
+    {
+        name: 'Tapete circular',
+        price: '0, 00',
+        image: '../image/tapete_circular.jpeg'
+    },
+    {
+        name: 'Rapete retangular',
+        price: '0, 00',
+        image: '../image/tapete_retangular.jpeg'
+    },
+    {
+        name: 'Suporte p/ vaso de planta',
+        price: '0, 00',
+        image: '../image/suporte_planta.jpeg'
+    }
+]
+
+document.onload(createItens())
+
+function createItens(){
+
+    const local = document.getElementById('local')
+    let div = document.createElement('div')
+    let image = document.createElement('img')
+    let name = document.createElement('h3')
+    let price = document.createElement('p')
+    let button = document.createElement('button')
+
+    for (let i = 0; i < products.length; i++){
+        image.setAttribute('src', products[i].image)
+        div.appendChild(image)
+        name.innerHTML = products[i].name
+        div.appendChild(name)
+        price.innerHTML = products[i].price
+        div.appendChild(price)
+        button.innerHTML = "Realizar compra"
+        div.appendChild(button)
+        local.appendChild(div)
+
+        // reinicalizando variaveis
+        div = document.createElement('div')
+        image = document.createElement('img')
+        name = document.createElement('h3')
+        price = document.createElement('p')
+        button = document.createElement('button')
+    }
+}
+
